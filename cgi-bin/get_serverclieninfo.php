bfa7d2ed6a70dddff110bb989f10a81711e959eb
Efstathiou G <y.efstathiou@gmail.com>
2014-05-09 11:32:48 +0300

    version & help
    manual server version info & additional info on upload page.

--- cgi-bin/get_serverclieninfo.php
+++ cgi-bin/get_serverclieninfo.php
@@ -3,10 +3,10 @@
 	$softchanges = 153;
 	$ver = [1,0,0,0];
 	$server_response = [];
-	$server_response['version'] = '';
-	
+	/*
 	// Calculation of "version string" (x.x.x.x) based on the number of file edits (not creations) since the beginning of the project
 	// Examples: 1234 translates to version 2.2.3.4, 4109 translates to version 5.1.0.9, 12078 translates to version 13.0.7.8
+	$server_response['version'] = '';
 	$i=0;
 	while($i<count($ver) && $softchanges > 0){
 		if(pow(10,(count($ver)-$i-1)) <= $softchanges){
@@ -21,9 +21,10 @@
 		$server_response['version'] = $server_response['version'] . '.' . $ver[$i];
 		$i++;
 	}
-	//
 	
 	$server_response['version'] = substr($server_response['version'],1);
+	*/
+	$server_response['version'] = '1.0 rc1';
 	$server_response['hostaddr'] = $_SERVER['REMOTE_ADDR'];
 	$server_response['hostname'] = gethostbyaddr($server_response['hostaddr']);
 	
