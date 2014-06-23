1e1a68ece71bd3f5312115a6ffc9e5500e01ecaf
Georgios Efstathiou <y.efstathiou@gmail.com>
2014-06-23 13:09:35 +0300

    - " -

--- cgi-bin/load_test_data.php
+++ cgi-bin/load_test_data.php
@@ -1,30 +1,81 @@
  <?php
-	//$test_data_dir = $_SERVER['DOCUMENT_ROOT'] . "/test data/";
+	/*
+	// DEBUG
 	$test_data_dir = "C:/Users/G Efstathiou/Documents/GitHub/ProteoSign/test data/";
-	$test_data_info_file = explode("\n", file_get_contents("$test_data_dir/info.txt"));
-	//$descriptions_requested = ($_POST["description"] === "true");
-	$descriptions_requested = true;
-	$server_response = [];
+	$descriptions_requested = false;
+	$dataset_requested = "dimethyl 2-plex (PD)";
+	*/
 	
-	for ($i=0; $i < count($test_data_info_file); $i++)  
-	{
-		$line_i = $test_data_info_file[$i];
-		$fields = explode(":", $line_i);
-		if($descriptions_requested){
-			$subfields = explode(" ", $fields[0]);
-			$files = $subfields[1];
-			$subfields = explode(" ", $fields[1]);
-			$desc = $subfields[2];
-			$server_response[] = $files . ":" . $desc;
-		}else{
-			/*
-			for ($j=0; $j < count($fields); $j++){
-				$subfields = explode(" ", $fields[$j]);
-			}
-			*/		
+	$test_data_dir = $_SERVER['DOCUMENT_ROOT'] . "/test data/";
+	$descriptions_requested = ($_POST["descriptions_requested"] === "true");
+	$dataset_requested = $_POST["dataset_info_requested"];
+	
+	$server_response = [];
+	$server_response['success'] = false;
+	$server_response['msg'] = "";
+	$server_response['queryres'] = [];
+	
+	try{
+		$db = new SQLite3("$test_data_dir/testdatadb");
+	}
+	catch (Exception $exception) {
+		$server_response['msg'] = "Failed to connect to the database: " . $exception->getMessage();
+		goto end;
+	}
+
+    if($descriptions_requested){
+		$qres = $db->query('select desc from dataset');
+	}else{
+		$qres = $db->query('select selector,value from dataset inner join param_value on dataset.id=param_value.dataset_id inner join param on param_value.param_id=param.id where dataset.desc="' . $dataset_requested . '"');
+	}
+	
+	if(!$qres){
+		$server_response['msg'] = "Failed to query the database: " . $sqlite->lastErrorMsg();
+		goto end;		
+	}
+    while($qrow = $qres->fetchArray(SQLITE3_ASSOC)){
+		foreach($qrow as $col => $val){
+			$server_response['queryres'][$col][] = $val;
+			//echo $col . ": " . $val . " ";
 		}
-	}	
- 
+		//echo "\n";
+	}
+	$qres->finalize();
+	
+	if(!$descriptions_requested){
+		$qres = $db->query('select file from dataset inner join dataset_files on dataset.id=dataset_files.dataset_id inner join files on dataset_files.file_id = files.id where dataset.desc="' . $dataset_requested . '"');
+		if(!$qres){
+			$server_response['msg'] = "Failed to query the database: " . $sqlite->lastErrorMsg();
+			goto end;		
+		}
+		while($qrow = $qres->fetchArray(SQLITE3_ASSOC)){
+			foreach($qrow as $col => $val){
+				$server_response['queryres'][$col][] = $val;
+				//echo $col . ": " . $val . " ";
+			}
+			//echo "\n";
+		}
+		$qres->finalize();		
+	}
+	
+	$db->close();
+	$server_response['success'] = true;
+	
+	/*
+	foreach($server_response['queryres']['selector'] as $key => $val){
+		echo $key . ": " . $val . " ";
+	}
+	echo "\n";	
+	foreach($server_response['queryres']['value'] as $key => $val){
+		echo $key . ": " . $val . " ";
+	}
+	echo "\n";	
+	foreach($server_response['queryres']['file'] as $key => $val){
+		echo $key . ": " . $val . " ";
+	}
+	echo "\n";
+	*/
+ end:
  	header('Content-type: application/json');
 	echo json_encode($server_response);	
 ?>
