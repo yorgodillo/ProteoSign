0a00566b9e77b8d06a520beb9d036edda92ddfcf
Georgios Efstathiou <y.efstathiou@gmail.com>
2015-04-01 12:56:25 +0300

    Forgot that

--- cgi-bin/MSdiffexp.R
+++ cgi-bin/MSdiffexp.R
@@ -1590,8 +1590,8 @@ paramssetfromGUI<-F
 working_directory<-getwd()
 limma_output<-"msdiffexp_out"
 LabelFree<-F
-source("/home/gefstathiou/Documents/ProteoSign/ProteoSign/uploads/1427878265781/msdiffexp_wd/MSdiffexp_definitions.R")
-#source("MSdiffexp_definitions.R")
+#source("/home/gefstathiou/Documents/ProteoSign/ProteoSign/uploads/1427878265781/msdiffexp_wd/MSdiffexp_definitions.R")
+source("MSdiffexp_definitions.R")
 
 perform_analysis<-function(){
   levellog("",change=1)
