98e814f1026ed0c175d09f931e4356e105b3a3d5
Georgios Efstathiou <y.efstathiou@gmail.com>
2015-04-30 11:53:26 +0300

    Performance improvement

--- cgi-bin/MSdiffexp.R
+++ cgi-bin/MSdiffexp.R
@@ -809,7 +809,6 @@ read.pgroups_v3<-function(fname,evidence_fname,time.point,keepEvidenceIDs=F){
   # E.g. 3: with 3 replicates, a protein that was quantified by two peptides in at least one of replicates will be discarded if 'nRequiredLeastBioreps' > 1 (retained otherwise).
   # E.g. 4: with 3 replicates, a protein that was quantified by two peptides (in total) in 2 out of the 3 replicates will be discarded if 'nRequiredLeastBioreps' > 2 (retained otherwise).
   
-  #Protein.IDs.quant = evidence.dt[, .(c1 = sum(n-nas)) , by=.(Protein.IDs, biorep)][, .(nQuantPeps = sum(c1), geqXnRequiredLeastBioreps = .N >= .GlobalEnv[["nRequiredLeastBioreps"]]), by=.(Protein.IDs)][nQuantPeps >= .GlobalEnv[["nRequiredLeastBioreps"]] & geqXnRequiredLeastBioreps == T][! Protein.IDs %like% '(K|k)eratin']$Protein.IDs
   Protein.IDs.quant <- evidence.dt[, .(c1 = sum(n-nas)) , by=.(Protein.IDs, biorep)][, .(nQuantPeps = sum(c1), geqXnRequiredLeastBioreps = .N >= .GlobalEnv[["nRequiredLeastBioreps"]]), by=.(Protein.IDs)][nQuantPeps >= .GlobalEnv[["nRequiredLeastBioreps"]] & geqXnRequiredLeastBioreps == T]$Protein.IDs
   levellog(paste0("read.pgroups_v3: Filtered out ", (length(unique(evidence.dt$Protein.IDs)) - length(Protein.IDs.quant))," proteins which were not identified in at least ",nRequiredLeastBioreps," biological replicate(s) with at least a total of ",nRequiredLeastBioreps," peptide(s)"));
   evidence.dt[,nQuantPeps := n-nas]
@@ -819,17 +818,17 @@ read.pgroups_v3<-function(fname,evidence_fname,time.point,keepEvidenceIDs=F){
   ## NOTE: It is applied when there are no technical replicates in Label-free data, where variability is expected to be very high.
   # If a protein intensity in condition i and biological replicate j is found to be an outlier based on the distribution
   # of intensities from all biological replicates, then
-  # the biological replicate j is removed for that particular protein in all conditions.
-  if(LabelFree && .GlobalEnv[["n_techreps"]] < 2){
-    evidence.dt.bad <- suppressWarnings(evidence.dt[, lapply(.SD, function(x){p.val = grubbs.test(x)$p.value; if(!is.na(p.val) && p.val < 0.05){outlier.true <- T}else{outlier.true <- F}; if(outlier.true){return(.I[outlier(x, logical=T)][1] )}else{return(as.integer(0))} }),by=.(Protein.IDs),.SDcols=paste0('Intensity.',conditions.labels[1])][,get(paste0('Intensity.',conditions.labels[1]))])
-    evidence.dt.bad <- evidence.dt.bad[evidence.dt.bad > 0]
-    evidence.dt<-evidence.dt[! evidence.dt.bad]
-    levellog(paste0("read.pgroups_v3: Filtered out ", length(evidence.dt.bad)," protein intensities based on outlier detection on condition '",conditions.labels[1],"'."));
-    Protein.IDs.quant <- evidence.dt[, .(c1 = sum(n-nas)) , by=.(Protein.IDs, biorep)][, .(nQuantPeps = sum(c1), geqXnRequiredLeastBioreps = .N >= .GlobalEnv[["nRequiredLeastBioreps"]]), by=.(Protein.IDs)][nQuantPeps >= .GlobalEnv[["nRequiredLeastBioreps"]] & geqXnRequiredLeastBioreps == T]$Protein.IDs
-    levellog(paste0("read.pgroups_v3: Filtered out another ", (length(unique(evidence.dt$Protein.IDs)) - length(Protein.IDs.quant))," proteins which were not identified in at least ",nRequiredLeastBioreps," biological replicate(s) with at least a total of ",nRequiredLeastBioreps," peptide(s)"));
-    evidence.dt[,nQuantPeps := n-nas]
-    evidence.dt<-evidence.dt[Protein.IDs %in% Protein.IDs.quant]
-  }
+  # the biological replicate j is removed for that particular protein for all conditions.
+  #if(LabelFree && .GlobalEnv[["n_techreps"]] < 2){
+  #  evidence.dt.bad <- suppressWarnings(evidence.dt[, lapply(.SD, function(x){p.val = grubbs.test(x)$p.value; if(!is.na(p.val) && p.val < 0.05){outlier.true <- T}else{outlier.true <- F}; if(outlier.true){return(.I[outlier(x, logical=T)][1] )}else{return(as.integer(0))} }),by=.(Protein.IDs),.SDcols=paste0('Intensity.',conditions.labels[1])][,get(paste0('Intensity.',conditions.labels[1]))])
+  #  evidence.dt.bad <- evidence.dt.bad[evidence.dt.bad > 0]
+  #  evidence.dt<-evidence.dt[! evidence.dt.bad]
+  #  levellog(paste0("read.pgroups_v3: Filtered out ", length(evidence.dt.bad)," protein intensities based on outlier detection on condition '",conditions.labels[1],"'."));
+  #  Protein.IDs.quant <- evidence.dt[, .(c1 = sum(n-nas)) , by=.(Protein.IDs, biorep)][, .(nQuantPeps = sum(c1), geqXnRequiredLeastBioreps = .N >= .GlobalEnv[["nRequiredLeastBioreps"]]), by=.(Protein.IDs)][nQuantPeps >= .GlobalEnv[["nRequiredLeastBioreps"]] & geqXnRequiredLeastBioreps == T]$Protein.IDs
+  #  levellog(paste0("read.pgroups_v3: Filtered out another ", (length(unique(evidence.dt$Protein.IDs)) - length(Protein.IDs.quant))," proteins which were not identified in at least ",nRequiredLeastBioreps," biological replicate(s) with at least a total of ",nRequiredLeastBioreps," peptide(s)"));
+  #  evidence.dt[,nQuantPeps := n-nas]
+  #  evidence.dt<-evidence.dt[Protein.IDs %in% Protein.IDs.quant]
+  #}
   
   
   
@@ -1069,20 +1068,24 @@ perform_analysis<-function(){
     unlink(limma_output, recursive=T, force=T)
   }
   dir.create(limma_output)
-  levellog("Removing double quotes from input data file #1 ...")
-  tmpdata<-gsub("\"", "", readLines(evidence_fname))
-  evidence_fname_cleaned<-file(evidence_fname, open="w")
-  writeLines(tmpdata, con=evidence_fname_cleaned)
-  close(evidence_fname_cleaned)
+  if(grepl("\"",readLines(evidence_fname, n=1))){
+    levellog("Removing double quotes from input data file #1 ...")
+    tmpdata<-gsub("\"", "", readLines(evidence_fname))
+    evidence_fname_cleaned<-file(evidence_fname, open="w")
+    writeLines(tmpdata, con=evidence_fname_cleaned)
+    close(evidence_fname_cleaned)
+  }
   levellog("Reading input data ...")
   if(PDdata){
     protein_groups<<-read.pgroups_v3(pgroups_fname,evidence_fname,time.point,keepEvidenceIDs=T)
   }else{
-    levellog("Removing double quotes from input data file #2 ...")
-    tmpdata<-gsub("\"", "", readLines(pgroups_fname))
-    pgroups_fname_cleaned<-file(pgroups_fname, open="w")
-    writeLines(tmpdata, con=pgroups_fname_cleaned)
-    close(pgroups_fname_cleaned)    
+    if(grepl("\"",readLines(pgroups_fname, n=1))){
+      levellog("Removing double quotes from input data file #2 ...")
+      tmpdata<-gsub("\"", "", readLines(pgroups_fname))
+      pgroups_fname_cleaned<-file(pgroups_fname, open="w")
+      writeLines(tmpdata, con=pgroups_fname_cleaned)
+      close(pgroups_fname_cleaned)
+    }
     protein_groups<<-read.pgroups_v3(pgroups_fname,evidence_fname,time.point,keepEvidenceIDs=T)
   }
   
